8e07a7110d9e08e40b6e8668ee990a0a7dbafa94
geunhokinn <gghh1220@gmail.com>
2024-05-15 15:56:06 +0900

    feat: 트랜잭션 기능 추가

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -12,6 +12,7 @@ public class MemberServiceV1 {
     private final MemberRepositoryV1 memberRepositoryV1;
 
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
         Member fromMember = memberRepositoryV1.findById(fromId);
         Member toMember = memberRepositoryV1.findById(toId);
 
@@ -19,6 +20,7 @@ public class MemberServiceV1 {
         // 계좌 이체 실패 예시
         validation(toMember);
         memberRepositoryV1.update(toId, fromMember.getMoney() + money);
+        // 트랜잭션 종료(커밋, 롤백)
     }
 
     private void validation(Member toMember) {
---
+++ src/main/java/hello/jdbc/service/MemberServiceV2.java
@@ -0,0 +1,68 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import hello.jdbc.repository.MemberRepositoryV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 파라미터 연동, 풀을 고려한 종료
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV2 {
+
+    private final DataSource dataSource;
+    private final MemberRepositoryV2 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Connection con = dataSource.getConnection();
+
+        try {
+            con.setAutoCommit(false); // 트랜잭션 시작(set autocommit false;)
+            // 비즈니스 로직 시작
+            bizLogic(con, fromId, toId, money);
+            // 비즈니스 로직 종료
+            con.commit(); // 트랜잭션 종료, 성공 -> 커밋
+        } catch (Exception e) {
+            con.rollback(); // 트랜잭션 종료, 실패 -> 롤백
+            throw new IllegalStateException(e);
+        } finally {
+            release(con);
+        }
+    }
+
+    private void bizLogic(Connection con, String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(con, fromId);
+        Member toMember = memberRepository.findById(con, toId);
+
+        memberRepository.update(con, fromId, fromMember.getMoney() - money);
+        // 계좌 이체 실패 예시
+        validation(toMember);
+
+        memberRepository.update(con, toId, fromMember.getMoney() + money);
+    }
+
+    private void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
