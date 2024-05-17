84ac45c909e90c01283f52443fa73f428bc7490c
geunhokinn <gghh1220@gmail.com>
2024-05-17 12:16:41 +0900

    feat: 예외에 의존하지 않는 서비스 추가

---
+++ src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,44 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 예외 누수 문제 해결
+ * SQLException 제거
+ * MemberRepository 인터페이스 의존
+ */
+@Slf4j
+public class MemberServiceV4 {
+
+    private final MemberRepository memberRepository;
+
+    public MemberServiceV4(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional // 트랜잭션 시작, 성공하면 커밋 실패 하면 롤백, 트랜잭션 종료
+    public void accountTransfer(String fromId, String toId, int money) {
+        // 비즈니스 로직
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        // 계좌 이체 실패 예시
+        validation(toMember);
+
+        memberRepository.update(toId, fromMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+}
